bb8495d242f1bf7c5074cf3310c574af1e8bc192
Ningning1046 <72305399+Ningning1046@users.noreply.github.com>
2023-08-17 15:25:18 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -6,9 +6,9 @@
 
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=28695949&pid=d31781bb791dc6af50f9890f83e28150&tid=6a99a4e7d64cd885bd02fdf9e2512e3d",
-    "https://www.maimemo.com/share/page?uid=28695949&pid=d31781bb791dc6af50f9890f83e28150&tid=6a99a4e7d64cd885bd02fdf9e2512e3d",
-    "https://www.maimemo.com/share/page?uid=28695949&pid=d31781bb791dc6af50f9890f83e28150&tid=6a99a4e7d64cd885bd02fdf9e2512e3d",
+    "",
+    "",
+    "",
   ]
 =====================以上为范例！=====================
 
@@ -20,9 +20,9 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=28695949&pid=9b7d6cfb587c326207ef06dddc5953b9&tid=c62c4e1b2d240f4e0e256199cf14c1cb",
-    "https://www.maimemo.com/share/page?uid=28695949&pid=9b7d6cfb587c326207ef06dddc5953b9&tid=c62c4e1b2d240f4e0e256199cf14c1cb",
-    "https://www.maimemo.com/share/page?uid=28695949&pid=9b7d6cfb587c326207ef06dddc5953b9&tid=c62c4e1b2d240f4e0e256199cf14c1cb",
+    "https://www.maimemo.com/share/page?uid=28695949&pid=d31781bb791dc6af50f9890f83e28150&tid=6a99a4e7d64cd885bd02fdf9e2512e3d",
+    "https://www.maimemo.com/share/page?uid=28695949&pid=d31781bb791dc6af50f9890f83e28150&tid=6a99a4e7d64cd885bd02fdf9e2512e3d",
+    "https://www.maimemo.com/share/page?uid=28695949&pid=d31781bb791dc6af50f9890f83e28150&tid=6a99a4e7d64cd885bd02fdf9e2512e3d",
   ]
   /**
    * 生成随机数字
